6ce1ff57bce2019ebd3d0d29e8673293268b56ad
Thomas Liebscher <tliebscher@unitybox.de>
2018-11-09 16:45:20 +0100

    removed class oraDocs

--- oradocs.py
+++ oradocs.py
@@ -57,25 +57,63 @@ class DocsDisplay(object):
         self.messagelb.pack(side=tk.LEFT, fill=tk.BOTH)
         self.messagefm.pack()
 
-        self.myOraDocs = OraDocs()
+        # self.myOraDocs = OraDocs()
         self.top.update()
+        self.readConfig()
 
     def printMsg(self, message):
         self.messagelb.insert(tk.END, message)
         self.top.update()
 
+    def readConfig(self, confFile=None):
+            # read the configuration file for example username password
+            # format =
+            # { "ssousername": "oracle_user_name",
+            #   "password": "oracle_SSO_password"}
+            #
+            # may other to come
+            #
+            if confFile is None:
+                confFile = './config.json'
+            with open(confFile) as json_file:
+                json_data = json.load(json_file)
+            try:
+                self.login_data = {}
+                self.login_data['ssousername'] = json_data['ssousername']
+                self.login_data['password'] = json_data['password']
+            except Exception as excp:
+                print("Exception during configuration %s" % (excp))
+                sys.exit
+            # debug print(self.login_data)
+
+    def openSession(self):
+        self.header = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux i686) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/36.0.1985.125 Safari/537.36"}
+        self.session = requests.Session()
+        self.session.headers.update(self.header)
+
+    def getAllInput(self, mySoup):
+        # retrieve all imput vars from an soup object
+        r_svars = {}
+        # pdb.set_trace()
+        for var in mySoup.findAll('input', type='hidden'):
+
+            r_svars[var['name']] = var['value']
+        return r_svars
+
     def doSSO(self, page):
         # do the SSO login here since we might need some manual action
-        counterSSO = 0
         self.printMsg('Doing SSO login')
 
         try:
             # do we find the login some where int the URL
             while "login" in page.url:
-                pdb.set_trace()
+                #pdb.set_trace()
                 soup = BeautifulSoup(page.content, 'html.parser')
                 allInput = {}
-                allInput = self.myOraDocs.getAllInput(soup)
+                allInput = self.getAllInput(soup)
                 # first check if we have the the internal login page
                 if soup.form.attrs['action'] == "https://login.oracle.com/mysso/signon.jsp":
                     # looks like we are doing SSO from oracle network
@@ -86,12 +124,12 @@ class DocsDisplay(object):
                     for key in allInput.keys():
                         payload[key] = allInput[key]
                         if key == "username":
-                            payload[key] = self.myOraDocs.login_data['ssousername']
+                            payload[key] = self.login_data['ssousername']
                         if key == "password":
-                            payload[key] = self.myOraDocs.login_data['password']
+                            payload[key] = self.login_data['password']
 
                     # print(payload)
-                    page = self.myOraDocs.session.post(login_url, data=payload)
+                    page = self.session.post(login_url, data=payload)
                 # second check if we have a "onload" in the body
                 elif soup.body['onload'] == "document.forms[0].submit();":
                     # if we have just a "press the submit button", doit
@@ -101,138 +139,22 @@ class DocsDisplay(object):
                             payload[key] = allInput[key]
                     # the url that requested that
                     post_url = soup.form.attrs['action']
-                    page = self.myOraDocs.session.post(post_url, data=payload)
+                    page = self.session.post(post_url, data=payload)
+                # third are we coming from outside
+                if soup.form.attrs['action'] == "https://login.oracle.com:443/oaam_server/oamLoginPage.jsp":
+                        # looks like we are coming from outside
+                        # the post url is like that /oam/server/sso/auth_cred_submit
+                        # so internal URL should be like that:
+                        login_url = soup.form.attrs['action']
+                        payload = {}
+                        payload = allInput
+                        # print(payload)
+                        page = self.session.post(login_url, data=payload)
+            return page
 
         except Exception as excp:
             myMessage = 'Houston we have a problem ' + str(excp)
             self.printMsg(myMessage)
-
-    def docsInit(self):
-        getToken = False
-        gotSSO = False
-        counterT = 0
-        while True:
-            counterT = counterT + 1
-            if counterT > 2:
-                self.printMsg('Failed to get Token')
-                break
-
-            self.printMsg('Getting Oracle Docs Token')
-            getToken, page = self.myOraDocs.getToken()
-            if getToken == 'SSO':
-                self.doSSO(page)
-
-    def getDocument(self):
-        docApi = 'api/1.2/folders/items'
-
-
-class OraDocs(object):
-    #
-    def __init__(self):
-        self.header = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux i686) \
-            AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/36.0.1985.125 Safari/537.36"}
-        self.session = requests.Session()
-        self.session.headers.update(self.header)
-        self.res = False  # session results
-        self.authToken = False  # our authentication token
-        self.readConfig()
-
-    def readConfig(self, confFile=None):
-        # read the configuration file for example username password
-        # format =
-        # { "ssousername": "oracle_user_name",
-        #   "password": "oracle_SSO_password"}
-        #
-        # may other to come
-        #
-        if confFile is None:
-            confFile = './config.json'
-        with open(confFile) as json_file:
-            json_data = json.load(json_file)
-        try:
-            self.login_data = {}
-            self.login_data['ssousername'] = json_data['ssousername']
-            self.login_data['password'] = json_data['password']
-        except Exception as excp:
-            print("Exception during configuration %s" % (excp))
-            sys.exit
-        # debug print(self.login_data)
-
-    def getAllInput(self, mySoup):
-        # retrieve all imput vars from an soup object
-        r_svars = {}
-        for var in mySoup.findAll('input', type='hidden'):
-            r_svars[var['name']] = var['value']
-        return r_svars
-
-    def checkSSOlogin(self, page):
-        # check if the page is a SSO login page.
-        login_success = False
-        while login_success is False:
-            soup = BeautifulSoup(page.content, 'html.parser')
-            # do we find the login.oracle.com
-            if "login.oracle.com" in soup.form.attrs['action']:
-                # check if we are coming from outside
-                if "oamLoginPage.jsp" in soup.form.attrs['action']:
-                    # looks like we need to login to OAM first
-                    print("OAM pre login_url")
-                    login_url = soup.form.attrs['action']
-                    payload = {}
-                    allInput = {}
-                    allInput = self.getAllInput(soup)
-                    for key in allInput.keys():
-                        payload[key] = allInput[key]
-                    page = self.session.post(login_url, data=payload)
-
-                # are we wait for SMS???
-                # if
-                # do the SSO login
-                # login_url = soup.form.attrs['action']
-                # allInput = {}
-                # allInput = self.getAllInput(soup)
-                #payload = {
-                #    'OAM_REQ': allInput['OAM_REQ'],
-                #    'locale': allInput['locale'],
-                #    'request_id': allInput['request_id'],
-                #    'contextType': allInput['contextType'],
-                #    'resource_url': allInput['resource_url'],
-                #}
-                #payload = {}
-                #for key in allInput.keys():
-                #     payload[key] = allInput[key]
-                ## print(payload)
-                #payload['userid'] = self.login_data['ssousername']
-                #payload['pass'] = self.login_data['password']
-                #pdb.set_trace()
-                #page = self.session.post(login_url, data=payload)
-            #else:
-                # looks like we don't have a login page
-                #login_success = True
-                #print("Login done or not needed")
-                #return page  # as it is (now)
-
-        return page  # return the new page
-
-    def checkOAMlogin(self, page):
-                # check if the page is a OAM login page.
-                # first make it beautiful
-                soup = BeautifulSoup(page.content, 'lxml')
-                # do we find the login.oracle.com
-                if "oraclecloud.com/oam/server" in soup.form.attrs['action']:
-                    # do the SSO login
-                    allInput = {}
-                    allInput = self.getAllInput(soup)
-                    payload = {}
-                    for var in allInput.keys():
-                        payload[var] = allInput[var]
-                    # pdb.set_trace()
-                    page = self.session.post(post_sso_url, data=payload)
-                    return page  # return the new page
-                else:
-                    print("no login needed")
-                    return page  # as it is
 
     def getToken(self):
         # try to get a token from the Oracle Docs server
@@ -246,94 +168,62 @@ class OraDocs(object):
         page = self.session.get(token_url)
         # pdb.set_trace()
         if page.status_code == requests.status_codes.codes.OK:
-            soup = BeautifulSoup(page.content, 'html.parser')
-            if 'login.oracle.com' in soup.form.attrs['action']:
-                return 'SSO', page
+            # if we find login in the URL we most likely need to do a login
+            if "login" in page.url:
+                page = self.doSSO(page)
+                if page.status_code != requests.status_codes.codes.OK:
+                    return "OUPS"
+            # either we reached here without need to login or we should be after
+            if "IdcService" in page.url:
+                self.printMsg('Getting Oracle Docs Token')
+                soup = BeautifulSoup(page.content, 'html.parser')
+                myJson = json.loads(str(soup))
+                self.authToken = myJson['LocalData']['tokenValue']
+                # update the header
+                self.header["Authorization"] = "Bearer %s" %self.authToken
+                self.session.headers.update(self.header)
+                # pdb.set_trace()
+                return self.authToken
 
-            allInput = self.getAllInput(soup)
+    def docsInit(self):
+        # gotSSO = False
+        gotToken = False
+        counterT = 0
+        # open a new session
+        self.openSession()
+        # get a authentication token
+        while True:
+            counterT = counterT + 1
+            if counterT > 4:
+                self.printMsg('Failed to get Token')
+                break
+            # pdb.set_trace()
+            gotToken = self.getToken()
+            if gotToken:
+                myMessage = 'Got Token' + str(gotToken)
+                self.printMsg(myMessage)
+                break
+        # get the root folder
+        self.getFolder('/')
 
-        return 'OAM', page
+    def getFolder(self, folder):
+        docApi = 'api/1.2/folders/items'
+        urlApi = doc_url + docApi
+        pdb.set_trace()
+        page = self.session.get(urlApi)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        myJson = json.loads(str(soup))
 
-    def getToken1(self):
-        # try to get the token from the OraDocs
-        # check if we have a token
-        docApi = '/web?IdcService=GET_OAUTH_TOKEN'
-        token_url = doc_url + docApi
 
-        if not self.authToken:
-            # try to open the web page
-            try:
-                # open the token page
-                self.page = self.session.get(token_url)
-                # check for problems
-                self.page.raise_for_status()
-                # if status is OK
-                if self.page.status_code == requests.status_codes.codes.OK:
-                    #pdb.set_trace()
-                    # check if we need to do a SSO login
-                    self.page = self.checkSSOlogin(self.page)
-                    self.page.raise_for_status()
-                    # just for the fun try it agin (debug)
-                    # self.page = self.checkSSOlogin(self.page)
-
-                    # check if we need to do a OAM login
-                    self.page = self.checkOAMlogin(self.page)
-                    self.page.raise_for_status()
-
-                    # we should have now the token page now try to extract
-                    soup = BeautifulSoup(self.page.content, 'html.parser')
-                    try:
-                        myJson = json.loads(str(soup))
-                        self.authToken = myJson['LocalData']['tokenValue']
-                        # update the header
-                        self.header["Authorization"] = "Bearer %s" %self.authToken
-                        self.session.headers.update(self.header)
-                        # pdb.set_trace()
-                        return self.authToken
-                        # print(self.authToken)
-                    except Exception as excp:
-                        print("Exception trying to get token %s" % (excp))
-
-                else:
-                    raise Exception("Something got wrong getting token_url")
-
-            except Exception as excp:
-                print("The folowing problem occured: %s" % (excp))
-        else:
-            print("we got already a token %s" % (self.authToken))
-            return self.authToken
-
-    def getFiles(self):
-        #doc_url = "https://oradocs-corp.documents.us2.oraclecloud.com/documents/api/1.2/folders/items"
-        page = self.session.get(doc_url)
-        print(page.content)
-
-    def getHomeFolder(self):
-        api = 'api/1.2/folders/items'
-        self.page = self.session.get(doc_url + api)
-        # pdb.set_trace()
-        if self.page.status_code == requests.status_codes.codes.OK:
-            soup = BeautifulSoup(self.page.content, 'html.parser')
-            myJson = json.loads(str(soup))
-            #pdb.set_trace()
-            return myJson
-
-    #def displayContent(self, myJson):
-    #    # display the content
+    def getDocument(self):
+        docApi = 'api/1.2/folders/items'
 
 
 def main():
-    # authToken = False
     d = DocsDisplay()
     d.docsInit()
+    d.getDocument()
     tk.mainloop()
-    # myOraDocs = OraDocs()
-    # myOraDocs.getToken()
-    # myDocs = myOraDocs.getHomeFolder()
-    # print(myDocs)
-    # print(myOraDocs.res.content)
-    # print(myOraDocs.page)
-    # print(myOraDocs.page.content)
 
 
 if __name__ == '__main__':
